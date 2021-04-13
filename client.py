0a0138d825df6e35511b09e7fe120ef6cfa1ba77
laellsr <lael_lsr@hotmail.com>
2021-04-13 16:13:05 -0300

    initial

---
+++ client.py
@@ -0,0 +1,51 @@
+import pygame
+from player import Player
+from network import Network
+
+width = 500
+height = 500
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
+
+clientNumber = 0
+
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+
+def redrawWindow(win,player, player2):
+    win.fill((255,255,255))
+    player.draw(win)
+    player2.draw(win)
+    pygame.display.update()
+
+
+def main():
+    run = True
+    n = Network()
+    startPos = read_pos(n.getPos())
+    p = Player(startPos[0],startPos[1],100,100,(0,255,0))
+    p2 = Player(0,0,100,100,(255,0,0))
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
+        p2.x = p2Pos[0]
+        p2.y = p2Pos[1]
+        p2.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+        p.move()
+        redrawWindow(win, p, p2)
+
+main()
